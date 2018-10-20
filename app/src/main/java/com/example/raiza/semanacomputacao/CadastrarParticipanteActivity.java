6770bdc0a7c5d036675deed08149badb7ec33cfc
Rodrigo Pituba de Souza <rodrigopitubasouza@live.com>
2018-10-20 17:45:06 -0300

    Cadastro de participante

--- app/src/main/java/com/example/raiza/semanacomputacao/CadastrarParticipanteActivity.java
+++ app/src/main/java/com/example/raiza/semanacomputacao/CadastrarParticipanteActivity.java
@@ -2,13 +2,42 @@ package com.example.raiza.semanacomputacao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class CadastrarParticipanteActivity extends AppCompatActivity {
+    private EditText edtNome;
+    private EditText edtEmail;
+    private EditText edtCpf;
+    private Button btnSalvar;
+    private TextView teste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ptc_cadastro_layout);
+        edtNome = (EditText) findViewById(R.id.ptc_edt_nome);
+        edtEmail = (EditText) findViewById(R.id.ptc_edt_email);
+        edtCpf = (EditText) findViewById(R.id.ptc_edt_cpf);
+
+        btnSalvar = findViewById(R.id.btn_ptc_salvar);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ListaParticipante.getInstance().add(new Participante(String.valueOf(edtNome.getText()),String.valueOf(edtEmail.getText()),String.valueOf(edtCpf.getText())));
+                Toast.makeText(CadastrarParticipanteActivity.this, edtNome.getText() + " Cadastrado com sucesso, O numero de cadastrados é " + ListaParticipante.getInstance().size(), Toast.LENGTH_SHORT).show();
+                edtCpf.setText("");
+                edtEmail.setText("");
+                edtNome.setText("");
+            }
+        });
+
 
     }
 }
